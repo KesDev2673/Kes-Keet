7c180ddc43575e244d4bbf5530f0860ea17410ac
KesDev2673 <ukeykesiena260703@gmail.com>
2024-10-21 13:28:52 +0100

    Addition of my Keet Project

---
+++ Assets/JS/app.js
@@ -0,0 +1,24 @@
+document.getElementById('copy-username').addEventListener('click', function() {
+    const username = '8764535373'; // The username you want to copy
+    navigator.clipboard.writeText(username).then(function() {
+        alert('User ID copied to clipboard!'); // Optional: provide feedback to the user
+    }).catch(function(err) {
+        console.error('Could not copy text: ', err);
+    });
+});
+
+document.getElementById("filter-icon").addEventListener("click", function() {
+    const contaElement = document.querySelector('.conta');
+    
+    // Toggle visibility of 'conta' element
+    if (contaElement.style.display === "none") {
+        contaElement.style.display = "block";
+    } else {
+        contaElement.style.display = "none";
+    }
+});
+
+document.querySelector('.hamburger-link').addEventListener('click', function() {
+    // Toggle the "open" class on the .menu element
+    document.querySelector('.menu').classList.toggle('open');
+});
